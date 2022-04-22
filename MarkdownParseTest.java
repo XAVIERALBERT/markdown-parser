da266cb15ae1ac0541924b6c1efc7c47988aa706
XAVIERALBERT <xav.al.cruz@gmail.com>
2022-04-22 12:54:41 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,13 +12,43 @@ public class MarkdownParseTest {
 
     @Test 
     public void testGetLinks() {
-        String toTest = 
-        "[link](https://rando.org/file-name) [link2](https://google.com) [link3](rando.gov)";
+        String toTest1 = 
+        "# Title [link1](https://something.com) [link2](some-thing.html)";
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        ArrayList<String> result = MarkdownParse.getLinks(toTest1);
+        assertEquals(expected,result);
+    }
+    @Test 
+    public void testGetLinks2() {
+        String toTest2 = 
+        "# Title2 [link](https://rando.org/file-name) [link2](https://google.com) [link3](rando.gov)";
 
         ArrayList<String> expected = new ArrayList<>();
         expected.add("https://rando.org/file-name");
         expected.add("https://google.com");
         expected.add("rando.gov");
+        ArrayList<String> result = MarkdownParse.getLinks(toTest2);
+        assertEquals(expected,result);
+    }
+    @Test 
+    public void testGetLinks3() {
+        String toTest3 = 
+        "[link]rando.com [link](rando.gov) [link](rando.org) link(rando.com)";
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("rando.gov");
+        expected.add("rando.org");
+        ArrayList<String> result = MarkdownParse.getLinks(toTest3);
+        assertEquals(expected,result);
+    }
+    @Test 
+    public void testGetLinks4() {
+        String toTest = "";
+
+        ArrayList<String> expected = new ArrayList<>();
         ArrayList<String> result = MarkdownParse.getLinks(toTest);
         assertEquals(expected,result);
     }
