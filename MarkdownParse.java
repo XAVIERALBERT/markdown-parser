025e389d078ad384479af2bb1afe42e4458317dd
XAVIERALBERT <xav.al.cruz@gmail.com>
2022-04-22 12:15:30 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -42,5 +42,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        System.out.println("this is a small change in the program.");
     }
 }
