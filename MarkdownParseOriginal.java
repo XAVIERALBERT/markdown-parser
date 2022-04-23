86061cd0f73d9097a627e349b6f3ebff22fba0e3
XAVIERALBERT <xav.al.cruz@gmail.com>
2022-04-22 20:03:12 -0700

    Update MarkdownParseOriginal.java

--- MarkdownParseOriginal.java
+++ MarkdownParseOriginal.java
@@ -12,13 +12,30 @@ public class MarkdownParseOriginal {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if (!markdown.contains("[")) {
+                return toReturn;
+            }
+            //if(!markdown.contains("[") || !markdown.contains("]")) {
+             //   break;
+            //}
+            //if(!markdown.contains("(") || !markdown.contains(")")) {
+            //    break;
+            //}
+            if ((markdown.contains("[") || markdown.contains("]")) && (markdown.contains("(") || markdown.contains(")"))) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if (!markdown.substring(currentIndex,markdown.length()).contains("(")) {
+                return toReturn;
+            } 
+            else if (!markdown.substring(currentIndex,markdown.length()).contains("[")) {
+                return toReturn;
+            } 
         }
+    }
 
         return toReturn;
     }
