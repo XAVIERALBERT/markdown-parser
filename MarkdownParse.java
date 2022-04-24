e6940cbe225c61e86726f94a2abedd974dd5de0c
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2022-04-24 16:27:16 -0700

    Update MarkdownParse.java
    added a way to distinguish between links and images (1/3 fixes)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,13 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            //to distinguish between image and link syntax
+            if (markdown.substring(openBracket-1, openBracket).equals("!")){
+            //do nothing
+            }
+            else{
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
 
