4084750edff5c8a78b1084c19f1b4d408d231051
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2022-04-29 12:25:14 -0700

    added junit test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
-/** import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 public class MarkdownParseTest {
+    
+    
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void getLinksTest() throws IOException {
+        Path fileName = Path.of("/Users/owen/Documents/GitHub/markdown-parser/test1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        assertEquals(expected, links);
     }
 }
-*/
