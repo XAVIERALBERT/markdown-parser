1650b9df20d6caf0eae2c2f7254f4f0e4819a288
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2022-05-06 12:53:21 -0700

    added makefile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,18 +10,20 @@ public class MarkdownParseTest {
     
     @Test
     public void getLinksTest() throws IOException {
-        Path fileName = Path.of("test1.md");
+        Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> expected = new ArrayList<>();
         expected.add("https://something.com");
+        expected.add("some-thing.html");
         assertEquals(expected, links);
 
-        Path fileName2 = Path.of("lab5_test.md");
+        /** Path fileName2 = Path.of("lab5_test.md");
         String content2 = Files.readString(fileName2);
         ArrayList<String> links2 = MarkdownParse.getLinks(content2);
         ArrayList<String> expected2 = new ArrayList<>();
         expected2.add("www.google.com");
         assertEquals(expected2, links2);
+        */
     }
 }
